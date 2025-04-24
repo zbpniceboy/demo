411db0555b9e3e003c94ce9358df5833b1d60075
zbpniceboy <zbpblog626@163.com>
2025-04-25 02:44:37 +0800

    2

--- src/stores/base.js
+++ src/stores/base.js
@@ -12,12 +12,12 @@ export const useBaseStore = defineStore('base', {
       this.user = res.data.user
     },
     async loginOut() {
-      await request.get('/logout')
+      await request.post('/logout')
       this.user = null
       this.hasAuth = false
     },
     async login(data) {
-      const res = await request.get('/login', data)
+      const res = await request.post('/login', data)
       if (res.data) {
         this.hasAuth = true
       } else {
